93f737b8afd97d2ae78bb5039b698458500ea47b
yomidepgit config --global user.name yomidepgit config --global user.name yomidep <noabiliaminfirst@gmail.com>
2023-08-23 21:03:23 +0100

    update

--- main.js
+++ main.js
@@ -1,88 +1,135 @@
 AOS.init();
 
-import ContractABI from "./contractABI.js";
-
 function useState(defaultValue) {
-  let value = defaultValue;
-
-  function getValue() {
-    return value;
-  }
-
-  function setValue(newValue) {
-    value = newValue;
-  }
-
-  return [getValue, setValue];
-}
-
-// Store the states here
-const [getConnected, setConnected] = useState(false);
-const [getAddress, setAddress] = useState("");
-
-// A function to connect the wallet
-const handleConnectMetamask = async () => {
-  try {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
-    const signer = await provider.getSigner();
-    const chainId = await signer.getChainId();
-
-    if (chainId !== 0x13881) { 
-      try {
-        await provider.send("wallet_switchEthereumChain", [
-          { chainId: 0x13881 } // Replace 1 with the desired chain ID
-        ]);
-      } catch (error) {
-        console.error("Error requesting account switch:", error);
-        return;
-      }
+    let value = defaultValue
+  
+    function getValue() {
+      return value
     }
   
-  } catch (error) {
-    console.error("Error connecting to Metamask:", error);
+    function setValue(newValue) {
+      value = newValue
+    }
+  
+    return [getValue, setValue];
   }
-};
-// Add an event listener to the button
-document.getElementById("Connect").addEventListener("click", handleConnectMetamask);
 
-const contractAddress = '0x7742A518d5dB7de7b37eb0590645ceBf575A6ffe'; //  contract addresss
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
-const contract = new ethers.Contract(contractAddress, ContractABI, signer);
+   // stored the states here
+   const [connected, setConnected] = useState(false);
+   const [address, setAddress] = useState("");
+ 
+   // a function to connect wallet
+   const handleConnectMetamask = async () => {
+     // used the try block just so i can catch errors easily
+     try {
+       // declared the provider here to search for window.ethereum
+       const provider = new ethers.providers.Web3Provider(window.ethereum);
+       // make a request to connect network
+       await provider.send("eth_requestAccounts", []);
+       // declared signer
+       const signer = await provider.getSigner();
+       // chainId of the connected network
+       const chainId = await signer.getChainId();
+ 
+       // we have a conditional block here that says if the user chainID isn't equal to that of the the Mumbai Network
+       if (chainId !== "0x13881") {
+         try {
+           // make a request to change the connected chain
+           await provider.send("wallet_switchEthereumChain", [
+             { chainId: `0x13881` },
+           ]);
+         } catch (error) {
+           // catcch error block
+           console.error("Error requesting account switch:", error);
+           return;
+         }
+       }
+ 
+       // a constant to get address
+       const address = await signer.getAddress();
+       // use the slice method to truncate address
+       const truncatedAddress = address.slice(0, 4) + ".." + address.slice(-2);
+       // set signer
+       setConnected(signer);
+       // set connected address
+       setAddress(truncatedAddress);
+     } catch (error) {
+       // catch error
+       console.log("Error Connecting: ", error);
+     }
+   };
 
-const voteButton = document.getElementById('vote');
+   const voteCandidate = async (event) => {
+    event.preventDefault();
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      let receipt;
+      const votingContract = new ethers.Contract(
+        contractAddress,
+        ContractABI,
+        signer
+      );
+      const voterInfo = await votingContract.voters(address);
+      const hasVoted = voterInfo.anyvotes;
+      console.log(hasVoted);
 
-voteButton.addEventListener('click', async () => {
-  const selectedProposal = document.getElementById('proposal').value;
-  try {
-    // Call the giveRightToVote function on the smart contract
-    await contract.giveRightToVote(signer.getAddress());
+      if (hasVoted) {
+        console.log("Already voted");
+      } else {
+        // Process the vote
+        console.log("Voting...");
+      }
 
-    // Call the vote function on the smart contract
-    const tx = await contract.vote(selectedProposal);
-    await tx.wait();
-    alert('Vote successful!');
-  } catch (error) {
-    console.error('Voting failed:', error);
-  }
-});
+      // Proceed with voting
+      const transaction = await votingContract.vote(value);
 
-const resultButton = document.getElementById('Result');
+      receipt = await wait(transaction);
 
-resultButton.addEventListener('click', async () => {
-  try {
-    // Call the winningName function on the smart contract
-    const result = await contract.winningName();
+      console.log("Vote submitted successfully!");
+      enqueueSnackbar("Vote Successful", { variant: "success" });
+    } catch (error) {
+      enqueueSnackbar(error.data.message, { variant: "error" });
+      console.log("Failed, reason: ", error.data.message);
+    }
+  };
 
-    // Display the result to the user
-    showResult(result);
-  } catch (error) {
-    console.error('Fetching result failed:', error);
-  }
-});
+  const VoteForm = () => {
+    const { enqueueSnackbar } = useSnackbar();
+    const contractAddress = "0x5CDf21c8072cDe0677e98BAD170d297C63a40cB1";
+  
+    const [value, setValue] = useState("");
+  
+    const handleInput = (event) => {
+      const inputValue = event.target.checked ? event.target.value : "";
+      setValue(inputValue);
+      console.log(inputValue);
+    };
+  
+    const winningCandidate = async () => {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const votingContract = new ethers.Contract(
+          contractAddress,
+          ContractABI,
+          signer
+        );
+        const winningName = await votingContract.winningName_();
+        const convertByte = ethers.utils.parseBytes32String(winningName);
+  
+        console.log(convertByte);
+        enqueueSnackbar(convertByte + " Project Is Leading", {
+          variant: "success",
+        });
+      } catch (error) {
+        console.log("Error Message: ", error.data);
+      }
+      
+    };
+    document.getElementById("winningButton").addEventListener("click", winningCandidate);
 
-async function showResult(result) {
-  // Display the result to the user in a modal dialog
-  // or notification
 }
